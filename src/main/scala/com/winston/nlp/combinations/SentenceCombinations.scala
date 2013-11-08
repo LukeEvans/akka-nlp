0af0c8f0b4fd9f337deb9842504a894e0c6032f6
keco1249 <kcolin1@gmail.com>
2013-11-08 15:37:22 -0700

    finished SentenceCombinations logic

--- src/main/scala/com/winston/nlp/combinations/SentenceCombinations.scala
+++ src/main/scala/com/winston/nlp/combinations/SentenceCombinations.scala
@@ -10,7 +10,7 @@ class SentenceCombinations {
 	var nlpSentences:ArrayList[NLPSentence] = new ArrayList[NLPSentence]
 	var scores:ArrayList[Double] = new ArrayList[Double]
 	var maxSentSeparation:Int = 4
-	var size:Int = 0
+	var size:Int = 20
 	var charLimit:Int = 300
 	
 	def this(size:Int){
@@ -127,15 +127,112 @@ class SentenceCombinations {
 	}
 	
 	def findHighestMMRCombo(scores: ArrayList[Double], combos: ArrayList[SentenceCombination]): SentenceCombination = {
+	  var combosForRemoval = new ArrayList[SentenceCombination];
+	  
+	  for(combo <- combos){
+	    if(combo.isOverMaxMMR()){
+	      combosForRemoval.add(combo)
+	    }
+	  }
+	    
+	  for(combo <- combosForRemoval){
+		 combos.remove(combo)
+	  }
+	    
+	  var returnCombo = new SentenceCombination()
+	    
+	  for(i <- 0 to combos.get(0).size){
+	     returnCombo.sentenceNumbers.add(-1)
+	  }
+	    
+	  for(i <- 0 to combos.size()){
+	    var score = getScore(scores, combos.get(i).sentenceNumbers)
+	    var highScore = getScore(scores, returnCombo.sentenceNumbers)
+	      
+	    if(highScore < score){
+	      returnCombo = combos.get(i)
+	    }
+	  }
 	  return new SentenceCombination()
 	}
 
 	def getCombosSentLimit(numbers:ArrayList[Int], min:Int, max:Int, n:Int, store: ArrayList[ArrayList[Int]]){
-	  
+	  if(n == 0){
+	    if(!tooSeparate(numbers, maxSentSeparation)){
+	      //copy
+	      var copy:ArrayList[Int] = numbers.clone().asInstanceOf[ArrayList[Int]];
+	      store.add(copy)
+	    }
+	  }
+	  else if(n > 0){
+	    for(i <- min to max){
+	      numbers.add(i)
+	      getCombosSentLimit(numbers, i+1, max, n-1, store)
+	      numbers.remove(numbers.size() - 1)
+	    }
+	  }
 	}
 	
 	def getCombosCharLimit(numbers:ArrayList[Int], min:Int, max:Int, n:Int, store: ArrayList[ArrayList[Int]], sentences:ArrayList[NLPSentence]){
+	  if(n > 0){
+	    for(i <- min to max){
+	      numbers.add(i)
+	      getCombosCharLimit(numbers, i+1, max, n-1, store, sentences)
+	      
+	      if(!tooSeparate(numbers, maxSentSeparation)){
+	        var sents = new ArrayList[NLPSentence]
+	        for(number <- numbers){
+	          sents.add(sentences.get(number))
+	        }
+	        
+	        if(comboWithinLimit(sents, charLimit)){
+	          var copy:ArrayList[Int] = numbers.clone().asInstanceOf[ArrayList[Int]]
+	          store.add(copy)
+	        }
+	      }
+	      numbers.remove(numbers.size() - 1)
+	    }
+	  }
+	}
+	
+	def tooSeparate(list:ArrayList[Int], difference:Int):Boolean = {
+	  var listSize = list.size()
+	  if(listSize == 0 || list == null){
+	    return true
+	  }
+	  else if(listSize == 1){
+	    return false
+	  }
+	  else{
+	    for(i <- 1 to listSize){
+	      if(getDifference(list.get(i), list.get(i-1)) > difference){
+	        return true
+	      }
+	    }
+	  }
+	  	return false
+	}
+	
+	def getDifference(a:Int, b:Int):Int= {
+	  return Math.abs(a -b)
+	}
+	
+	def comboWithinLimit(sentenceList:ArrayList[NLPSentence], limit:Int):Boolean = {
+	  if(sentenceList == null){
+	    return false
+	  }
+	  var combinedCharCount = 0
 	  
+	  for(sentence <- sentenceList){
+	    combinedCharCount += sentence.value.length()
+	  }
+	  
+	  if((combinedCharCount < (limit + 50)) && (combinedCharCount > (limit - 50))){
+	    return true
+	  }
+	  else{
+	    return false
+	  }
 	}
 	
 	def getScore(scores:ArrayList[Double], combo: ArrayList[Int]):Double = {
